946e16d013d7b825025557cf8bc5fcc72ab57819
olga <sinukova.olga@gmail.com>
2025-05-02 23:10:45 +0300

    add Logger

--- src/test/java/main/BaseSuite.java
+++ src/test/java/main/BaseSuite.java
@@ -2,6 +2,8 @@ package main;
 
 import data.BrowserModeData;
 import factory.WebDriverFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
@@ -9,6 +11,7 @@ import org.openqa.selenium.WebDriver;
 import java.time.Duration;
 
 public class BaseSuite {
+    protected static final Logger logger = LogManager.getLogger("Suite Logger");
 
     protected  WebDriver createDriver(BrowserModeData mode){
         WebDriver driver = new WebDriverFactory().getDriver(mode);
--- src/test/java/main/TestSuite.java
+++ src/test/java/main/TestSuite.java
@@ -1,6 +1,8 @@
 package main;
 
 import data.BrowserModeData;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.EnumSource;
@@ -8,35 +10,40 @@ import org.openqa.selenium.WebDriver;
 import pages.MainPage;
 
 public class TestSuite extends BaseSuite{
-
     @ParameterizedTest
     @EnumSource(value = BrowserModeData.class, names = {"HEADLESSSCREEN"})
     public void checkInputTextLabel(BrowserModeData mode) {
+        logger.info(String.format("Run test checkInputTextLabel with %s mode", mode));
         WebDriver driver = createDriver(mode);
         MainPage mainPage = new MainPage(driver);
         mainPage.open();
         mainPage.inputTextLabel();
         driver.quit();
+        logger.info("Finish test checkInputTextLabel");
     }
 
     @ParameterizedTest
     @EnumSource(value = BrowserModeData.class, names = {"KIOSKSCREEN"})
     public void checkOpeningModalWindow(BrowserModeData mode) {
+        logger.info(String.format("Run test checkOpeningModalWindow with %s mode", mode));
         WebDriver driver = createDriver(mode);
         MainPage mainPage = new MainPage(driver);
         mainPage.open();
         mainPage.openingModalWindow();
         driver.quit();
+        logger.info("Finish test checkOpeningModalWindow");
     }
 
     @ParameterizedTest
     @EnumSource(value = BrowserModeData.class, names = {"FULLSCREEN"})
     public void checkSubmitSampleForm(BrowserModeData mode) {
+        logger.info(String.format("Run test checkSubmitSampleForm with %s mode", mode));
         WebDriver driver = createDriver(mode);
         MainPage mainPage = new MainPage(driver);
         mainPage.open();
         mainPage.submitSampleForm();
         driver.quit();
+        logger.info("Finish test checkSubmitSampleForm");
     }
 }
 
